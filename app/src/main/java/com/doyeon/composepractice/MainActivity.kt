bb724ce45e97f4ade6847264834b8574df24680c
dddooo9 <dlaehdus99@naver.com>
2022-05-13 00:23:55 +0900

    [STYLE] Material Color 적용

--- app/src/main/java/com/doyeon/composepractice/MainActivity.kt
+++ app/src/main/java/com/doyeon/composepractice/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
@@ -46,12 +47,16 @@ fun MessageCard(msg: Message) {
             modifier = Modifier
                 .size(40.dp)
                 .clip(CircleShape)
+                .border(1.5.dp, MaterialTheme.colors.secondary, CircleShape)
         )
 
         Spacer(modifier = Modifier.width(8.dp))
 
         Column {
-            Text(text = msg.author)
+            Text(
+                text = msg.author,
+                color = MaterialTheme.colors.secondaryVariant
+            )
             Spacer(modifier = Modifier.height(4.dp))
             Text(text = msg.body)
         }
